7b505c312b4c252ae8806395e475ddadb3401d82
Jay Heng <jie.heng@nxp.com>
2018-11-18 16:43:54 +0800

    [MEM] Mark Auth block (CSF, DEK KeyBlob) data for NOR device
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- src/mem/memdef.py
+++ src/mem/memdef.py
@@ -10,5 +10,13 @@ kMemBlockSize_EKIB       = 0x20
 kMemBlockSize_EPRDB      = 0x100
 kMemBlockSize_IVT        = 0x20
 kMemBlockSize_BootData   = 0x10
+kMemBlockSize_CSF        = 0x1000
 kMemBlockSize_KeyBlob    = 0x200
+
+kMemberOffsetInIvt_Hdr      = 0x00
+kMemberOffsetInIvt_Entry    = 0x04
+kMemberOffsetInIvt_Dcd      = 0x0a
+kMemberOffsetInIvt_BootData = 0x10
+kMemberOffsetInIvt_Self     = 0x14
+kMemberOffsetInIvt_Csf      = 0x18
 

1e0c93eaacc335ec593e3e07bb13265c85986ddd
Jay Heng <jie.heng@nxp.com>
2018-12-05 13:58:05 +0800

    [GEN] all bat files support path with blank space
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- src/main.py
+++ src/main.py
@@ -586,7 +586,7 @@ if __name__ == '__main__':
     app = wx.App()
 
     main_win = secBootMain(None)
-    main_win.SetTitle(u"nxpSecBoot v0.10.1")
+    main_win.SetTitle(u"nxpSecBoot v0.10.2")
     main_win.Show()
 
     app.MainLoop()

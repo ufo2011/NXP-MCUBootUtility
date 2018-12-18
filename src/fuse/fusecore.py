d833a3239b4cc242d61ebb395286c60c8feb32f6
Jay Heng <jie.heng@nxp.com>
2018-12-18 14:48:25 +0800

    [FUSE] Only re-scan the fuse which has just been burned ('Burn' button)
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- src/fuse/fusecore.py
+++ src/fuse/fusecore.py
@@ -12,9 +12,11 @@ class secBootFuse(runcore.secBootRun):
     def __init__(self, parent):
         runcore.secBootRun.__init__(self, parent)
 
+        self.needToScanFuse = None
         self.scannedFuseList = [None] * fusedef.kMaxEfuseWords
         self.toBeBurnnedFuseList = [None] * fusedef.kMaxEfuseWords
         self.runModeFuseFlagList = [None] * fusedef.kMaxEfuseWords
+        self.toBeRefreshedFuseList = [False] * fusedef.kMaxEfuseWords
         self.isRunModeFuseFlagRemapped = False
 
         self.applyFuseOperToRunMode()
@@ -37,6 +39,7 @@ class secBootFuse(runcore.secBootRun):
         self._initEntryModeFuseFlag()
         self.updateFuseRegionField()
         self.isRunModeFuseFlagRemapped = False
+        self.needToScanFuse = True
 
     def _remapRunModeFuseFlagList( self ):
         if self.isRunModeFuseFlagRemapped:
@@ -57,13 +60,22 @@ class secBootFuse(runcore.secBootRun):
         else:
             pass
 
-    def scanAllFuseRegions( self, needSwapAndShow=True ):
+    def scanAllFuseRegions( self, needSwapAndShow=True, isRefreshOpt=False ):
+        self.needToScanFuse = False
+        hasRefreshFuse = False
         self._remapRunModeFuseFlagList()
         for i in range(fusedef.kMaxEfuseWords):
             if self.runModeFuseFlagList[i]:
-                self.scannedFuseList[i] = self.readMcuDeviceFuseByBlhost(fusedef.kEfuseIndex_START + i, '', False)
+                if not isRefreshOpt:
+                    self.scannedFuseList[i] = self.readMcuDeviceFuseByBlhost(fusedef.kEfuseIndex_START + i, '', False)
+                elif self.toBeRefreshedFuseList[i]:
+                    self.scannedFuseList[i] = self.readMcuDeviceFuseByBlhost(fusedef.kEfuseIndex_START + i, '', False)
+                    self.toBeRefreshedFuseList[i] = False
+                    hasRefreshFuse = True
             else:
                 self.scannedFuseList[i] = None
+        if isRefreshOpt and (not hasRefreshFuse):
+            return
         if needSwapAndShow:
             self._swapRemappedScannedFuseIfAppliable()
             self.showScannedFuses(self.scannedFuseList)
@@ -80,7 +92,10 @@ class secBootFuse(runcore.secBootRun):
         self.toBeBurnnedFuseList = self.getUserFuses()
         self._swapRemappedToBeBurnFuseIfAppliable()
         self._remapRunModeFuseFlagList()
-        self.scanAllFuseRegions(False)
+        if self.needToScanFuse:
+            self.scanAllFuseRegions(False)
+        else:
+            self._swapRemappedScannedFuseIfAppliable()
         for i in range(fusedef.kMaxEfuseWords):
             if self.runModeFuseFlagList[i]:
                 if self.toBeBurnnedFuseList[i] != self.scannedFuseList[i] and \
@@ -88,4 +103,5 @@ class secBootFuse(runcore.secBootRun):
                    self.scannedFuseList[i] != None:
                     fuseValue = self.toBeBurnnedFuseList[i] | self.scannedFuseList[i]
                     self.burnMcuDeviceFuseByBlhost(fusedef.kEfuseIndex_START + i, fuseValue)
-        self.scanAllFuseRegions()
+                    self.toBeRefreshedFuseList[i] = True
+        self.scanAllFuseRegions(True, True)

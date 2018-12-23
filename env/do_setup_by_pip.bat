9f3c1cdd0d8bb782a6e535fad92d09a120000bf2
Jay Heng <jie.heng@nxp.com>
2018-12-23 19:35:57 +0800

    [ENV] Add do_setup_by_pip.bat to build dev environment
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

---
+++ env/do_setup_by_pip.bat
@@ -0,0 +1,7 @@
+pip.exe install wxPython==4.0.3
+pip.exe install pyserial==3.4
+pip.exe install pywinusb==0.4.2
+pip.exe install bincopy==15.0.0 
+
+pip.exe install PyInstaller
+pip.exe install cx_Freeze

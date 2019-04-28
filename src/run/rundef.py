958578f4978616f36ef49344db2ca9350de548ec
Jay Heng <jie.heng@nxp.com>
2019-04-28 15:49:27 +0800

    NSB-27 Can generate .sb file by actions in efuse operation utility window
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- src/run/rundef.py
+++ src/run/rundef.py
@@ -3,6 +3,9 @@ import sys, os
 kUartSpeed_Blhost = ['115200', '57600', '19200', '9600', '4800']
 kUartSpeed_Sdphost = ['115200']
 
+kActionFrom_AllInOne = 0x0
+kActionFrom_BurnFuse = 0x1
+
 kBootDeviceMemId_SemcNor      = 0x8
 kBootDeviceMemId_FlexspiNor   = 0x9
 kBootDeviceMemId_SemcNand     = 0x100

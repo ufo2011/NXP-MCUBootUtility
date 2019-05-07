be169785b9fdfc76b69741ebd269a92281966186
Jay Heng <jie.heng@nxp.com>
2019-05-07 21:11:53 +0800

    NSB-29 Provide friendly way to view and set mixed eFuse fields #9 MiscConf0 Improvement
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- src/targets/MIMXRT1021/bltargetconfig.py
+++ src/targets/MIMXRT1021/bltargetconfig.py
@@ -59,6 +59,7 @@ efuse_0x400_bit14 = {'SRK':      ['0 - Unlock', '1 - W,0,RP']}
 efuse_0x400_bit15 = {'OTPMK_MSB':['0 - Unlock', '1 - W,0,RP']}
 efuse_0x460_bit13_12 = {'BEE_KEY0_SEL':['00 - From Register', '01 - Reserved', '10 - From OTPMK', '11 - From SW-GP2']}
 efuse_0x460_bit15_14 = {'BEE_KEY1_SEL':['00 - From Register', '01 - Reserved', '10 - From OTPMK', '11 - From SW-GP2']}
+efuse_0x460_bit31_30 = {'SD_BT_Power_Cycle_SEL':['00 - 20ms', '01 - 10ms', '10 - 5ms', '11 - 2.5ms']}
 efuseDescDiffDict = {'0x400_lock_bit7' :   uidef_efuse.efuse_temp_reserved1,
                      '0x400_lock_bit14':   efuse_0x400_bit14,
                      '0x400_lock_bit15':   efuse_0x400_bit15,
@@ -67,6 +68,8 @@ efuseDescDiffDict = {'0x400_lock_bit7' :   uidef_efuse.efuse_temp_reserved1,
                      '0x400_lock_bit25_24':uidef_efuse.efuse_temp_reserved2,
                      '0x460_bootcfg1_bit13_12':efuse_0x460_bit13_12,
                      '0x460_bootcfg1_bit15_14':efuse_0x460_bit15_14,
+                     '0x460_bootcfg1_bit31_30':efuse_0x460_bit31_30,
+                     '0x6d0_miscconf0_bit19_16':uidef_efuse.efuse_0x6d0_flexramPartion256KB,
                     }
 
 # memory map

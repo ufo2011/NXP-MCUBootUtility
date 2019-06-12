499dc1b49e6a4251265b3d6c6ff4ee9e0994a810
Jay Heng <jie.heng@nxp.com>
2019-06-12 23:42:53 +0800

    NSB-33 Support new MCU device - RT1170 A0 #3 Pass DEV boot mode test
    - Fixed minor flexspi nor cfg offset issue for RT10yy
    
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- src/targets/MIMXRT1015/bltargetconfig.py
+++ src/targets/MIMXRT1015/bltargetconfig.py
@@ -59,7 +59,7 @@ availableBootDevices = [RTyyyy_uidef.kBootDevice_FlexspiNor,
                         RTyyyy_uidef.kBootDevice_LpspiNor]
 flexspiNorDevice = uidef.kFlexspiNorDevice_Adesto_AT25SF128A
 flexspiNorMemBase = 0x60000000
-xspiNorCfgInfoOffset = 0x1000
+xspiNorCfgInfoOffset = 0x0
 flexspiNorEfuseBootCfg0Bits = 10
 isNonXipImageAppliableForXipableDeviceUnderClosedHab = False
 isSipFlexspiNorDevice = False
